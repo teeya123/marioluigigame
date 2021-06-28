af6d4a27eab55265fd1d02513800e22948bf4d5e
teeya123 <73390143+teeya123@users.noreply.github.com>
2021-06-28 18:34:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_die=loadSound("mariodie.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
 	setSprites();
 	MarioAnimation();
 }
